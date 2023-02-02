467628b29f8e3c21829b51dac2a6a8a170b7d866
huytuong010101 <huytuong010101@gmail.com>
2023-02-02 22:38:25 +0700

    fix/missing cont

--- src/database/init_db.js
+++ src/database/init_db.js
@@ -1,7 +1,7 @@
-db = require('./connection.js')
+const db = require('./connection.js')
 
-create_sql = `CREATE TABLE user ( id INT NOT NULL AUTO_INCREMENT , name VARCHAR(255) NOT NULL , age INT unsigned, gender BOOLEAN , PRIMARY KEY (id))`
-seed_sql = `INSERT INTO user(name, age, gender) values ('Nguyen Van A', 18, true), ('Nguyen Van B', 20, false), ('Tran Thi C', 10, true)`
+const create_sql = `CREATE TABLE user ( id INT NOT NULL AUTO_INCREMENT , name VARCHAR(255) NOT NULL , age INT unsigned, gender BOOLEAN , PRIMARY KEY (id))`
+const seed_sql = `INSERT INTO user(name, age, gender) values ('Nguyen Van A', 18, true), ('Nguyen Van B', 20, false), ('Tran Thi C', 10, true)`
 
 db.query(create_sql, (err, result) => {
     if (err){
