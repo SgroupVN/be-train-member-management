1ae92834d8dd9feead6475a24d492a4111810030
dangphu2412 <dangphu241299@gmail.com>
2023-02-05 09:49:38 +0700

    feat: setup lint

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
-const userRouter = require("./users/router");
+const userRouter = require('./users/router');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/users', userRouter);
 
