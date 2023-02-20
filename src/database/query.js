459919abc0c40100870dd9a8d9c1104a45e5b3ad
PVCuong2001 <cuongphanit2001@gmail.com>
2023-02-20 22:34:57 +0700

    feat(reset-password): init reset password

--- src/database/query.js
+++ src/database/query.js
@@ -1,6 +1,6 @@
 const executeQuery = ({ db, query, params }) => {
   return new Promise((resolve, reject) => {
-    db.query(query, [params], (err, rows) => {
+    db.query(query, params, (err, rows) => {
       if (err) {
         reject(err);
       }
@@ -8,6 +8,15 @@ const executeQuery = ({ db, query, params }) => {
       resolve(rows);
     });
   });
+};
+
+const updateOne = async ({ db, query, params }) => {
+  const result = await executeQuery({ db, query, params });
+  if (result.affectedRows > 0) {
+    return true;
+  }
+
+  return false;
 };
 
 const getOne = async ({ db, query, params }) => {
@@ -29,5 +38,6 @@ const getMany = async ({ db, query, params }) => {
 module.exports = {
   getOne,
   getMany,
+  updateOne,
   executeQuery,
 };
